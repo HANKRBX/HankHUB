f551bf13fc4434114bf689f6a394cf2e219a6919
H4NKRBX <79016427+HANKRBX@users.noreply.github.com>
2021-07-19 21:13:12 +0700

    Updated

--- [HH] Hank HUB How to use HankLib.lua
+++ [HH] Hank HUB How to use HankLib.lua
@@ -1,7 +1,4 @@
-local Library =
-    loadstring(
-    game:HttpGet("https://raw.githubusercontent.com/HANKRBX/HankHUB/main/%5BHH%5D%20Hank%20HUB%20HankLib.lua", true)
-)() -- Library Loadstring
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/CAtbYQ0Y", true))() -- Library Loadstring
 
 local Window = Library:Window("YourHUBName", "Adopt Me Scam") -- Create a new Window and Main Title
 
@@ -14,13 +11,13 @@ NewTab:Label("Label") -- Create a Label
 NewTab:Button( -- Create a Button
     "Button", -- Name
     function()
-        print("Button Pressed")
+        print("Button Pressed") -- Printed After Button Pressed
     end
 )
 
 NewTab:Toggle( -- Create a Toggle Button
     "Toggle", -- Name
-    function(toggle) 
+    function(toggle) -- callback toggle
         if toggle == true then -- When toggle on
             print("Toggle On")
         else -- When toggle off
@@ -33,15 +30,15 @@ NewTab:Slider( -- Create a Slider
     "Slider", -- Name
     16, -- Minimal Value
     500, -- Maximum Value
-    function(value)
+    function(value) -- callback value
         game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid").WalkSpeed = value -- For example how is it works
     end
 )
 
 NewTab:TextBox( -- Create a TextBox
     "TextBox", -- Name
-    function(text)
-        print("Text")
+    function(text) -- callback text
+        print(text) -- Print TextBox Text
     end
 )
 
@@ -49,6 +46,14 @@ NewTab:Dropdown( -- Create a TextBox
     "Dropdown", -- Name
     {"Button1", "Button2", "Button3"}, -- List
     function(v) -- Create a Dropdown, and Add Button on List "{"Button1", "Button2", "Button3"}"
-        print(v)
+        print(v) -- Print Dropdown List
+    end
+)
+
+NewTab:ColorPicker( -- Create a Color Picker
+    "ColorPicker", -- Name
+    Color3.fromRGB(25, 25, 25), -- Default Color
+    function(color) -- callback color
+        print(color) -- Print color
     end
 )
